9a40d99253a1e9fdba58ee33beb3fc2821c3d314
Catherine Miguel <catmig1992@gmail.com>
2022-11-12 02:19:37 -0800

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,19 +3,23 @@ const breads = express.Router();
 const Bread = require("../models/bread.js");
 
 // INDEX
-breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: Bread
-      }
-    )
+breads.get("/", (req, res) => {
+  res.render("Index", {
+    breads: Bread,
+  });
   // res.send(Bread)
-})
-
+});
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  res.send(Bread[req.params.arrayIndex])
-})
+breads.get("/:arrayIndex", (req, res) => {
+  // res.send(Bread[req.params.arrayIndex])
+  if (Bread[req.params.arrayIndex]) {
+    res.render("Show", {
+      bread: Bread[req.params.arrayIndex],
+    });
+  } else {
+    res.send("404");
+  }
+});
 
 module.exports = breads;
--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const PORT = process.env.PORT;
 const app = express();
 
 // MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
 
 // ROUTES
 app.get("/", (req, res) => {
@@ -17,8 +18,13 @@ app.get("/", (req, res) => {
 });
 
 // Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+const breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
+
+// 404 Page
+app.get("*", (req, res) => {
+  res.send("404");
+});
 
 // LISTEN
 app.listen(PORT, () => {
