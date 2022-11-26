2e46eb1cf10d82849fbbb37f6d16de4d12c07fa2
Catherine Miguel <catmig1992@gmail.com>
2022-11-25 23:50:44 -0800

    created and implemented virtuals

--- views/index.jsx
+++ views/index.jsx
@@ -1,18 +1,30 @@
 const React = require("react");
 const Default = require("./layouts/Default");
 
-function Index({ breads }) {
+// function Index({ breads }) {
+function Index({ breads, bakers, title }) {
   return (
-    <Default>
+    // <Default>
+    <Default title={title}>
       <h2>Index Page</h2>
       {/* <p>I have {breads[0].name} bread!</p> */}
       {/* This is a JSX comment. */}
+      <h3>Bakers</h3>
+      <ul>
+        {bakers.map((baker) => {
+          return (
+            <li key={baker.id}>
+              <a href={`/bakers/${baker.id}`}>{baker.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+      <h3>Breads</h3>
       <div className="newButton">
         <a href="/breads/new">
           <button>Add a new bread</button>
         </a>
       </div>
-
       <ul>
         {breads.map((bread, index) => {
           return (
